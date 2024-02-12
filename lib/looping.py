62be7734d27755ad2244c2c737640b7868ecf4d6
connorq <connorquitt@gmail.com>
2024-02-12 14:06:25 -0600

    all tests passing

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 0
+    while i <= 10:
+        print(i)
+        i += 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [int * int for int in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 0
+    while i <= 100:
+        if i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz')
+        elif i % 3 == 0:
+            print('Fizz')
+        elif i % 5 == 0:
+            print('Buzz')
+        else:
+            print(i)
